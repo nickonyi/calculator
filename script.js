dbbb65cb37cfd0eee7dfa7a3fad86d75828f2745
nickonyi <nickoluoch1130@gmail.com>
2022-07-15 06:47:02 +0300

    calculator

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-const screenLast = document.getElementById("lastOperationScreen");
-const screenCurrent = document.getElementById("currentoperationscreen");
-const allBtns = document.querySelector('.all-button');
-allBtns.forEach(btn => btn.addEventListener('click', ))
-screenLast = allBtn.textContent;
+const previousOperandTextElement = document.querySelector("[data-type='previous-operand']");
+const currentOperandTextElement = document.querySelector("[data-type='current-operand']");
+const numbersBtn = document.querySelectorAll("[data-type='number']");
+const operatorsBtn = document.querySelectorAll("[data-type='operator']");
+const allClearBtn = document.querySelector("[data-type='reset']");
+const clearBtn = document.querySelector("[data-type='backspace']");
+const decimalBtn = document.querySelector("[data-type='decimal']");
+const equalsBtn = document.querySelector("[data-type='equal']");
+
+
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentoperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    appendNumber(number) {
+
+    }
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand;
+    }
+}
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numbersBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        calculator.appendNumber(btn.textContent);
+        calculator.updateDisplay();
+    });
+});
+
+
+console.log(calculator);
+
 
 function add(a, b) {
     return a + b;
