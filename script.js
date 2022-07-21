7b1034effc4626f6c52876def65fa1389adbc9ad
nickonyi <nickoluoch1130@gmail.com>
2022-07-21 06:04:01 +0300

    Calculator

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ class Calculator {
     }
 
     clear() {
-        this.currentOperand = '';
+        this.currentOperand = '0';
         this.previousOperand = '';
         this.operation;
     }
@@ -26,15 +26,45 @@ class Calculator {
         if (number === "." && this.currentOperand.includes('.')) return;
         this.currentOperand = this.currentOperand.toString() + number.toString();
     }
+
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
     updateDisplay() {
         this.currentoperandTextElement.textContent = this.getDisplayNumber(this.currentOperand);
 
         if (this.operation != null) {
             this.previousOperandTextElement.textContent = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
         } else {
-            this.previousOperandTextElement = "";
+            this.previousOperandTextElement.textContent = "";
         }
+        let maxSize = this.currentoperandTextElement.textContent;
+
+
+        if (maxSize.length > 16) {
+            this.clear();
+        }
+
+
     }
+
+
+
 
     chooseOperation(operation) {
         if (this.currentOperand === "") return;
@@ -72,6 +102,8 @@ class Calculator {
                 return;
         }
 
+
+
         this.currentOperand = computation;
         this.operation = undefined;
         this.previousOperand = "";
@@ -80,14 +112,6 @@ class Calculator {
 
     delete() {
         this.currentOperand = this.currentOperand.toString().slice(0, -1);
-    }
-
-
-    getDisplayNumber(number) {
-        const floatNumber = parseFloat(number);
-
-        if (isNaN(floatNumber)) return '';
-        return floatNumber.toLocaleString('en');
     }
 }
 
